0034a26998280f785e32e7bb32243c617f4a7184
Sachin Mundhe <sachin.mundhe@ibm.com>
2021-12-08 14:34:30 +0530

    release

---
+++ release.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    branches: "master",
+    repositoryUrl: "https://github.com/abhishek-devani/react-app",
+    plugins: ["@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
+    ]
+}
